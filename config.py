2a9d187767fa0cfa2ddf299a67b7800ecaa3911a
Dovahkiin2333-lnx <alanxu2333@outlook.com>
2025-05-01 10:14:05 +0100

    update config

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+
+SECRET_KEY = "asdfkasjdhf"
+
+WTF_CSRF_ENABLED = True  
+WTF_CSRF_SECRET_KEY = "ziasdyfgfmfdy"
+
+
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+USERNAME = 'root'
+PASSWORD = '1dcda0446167059c'
+# DATABASE = 'musicweb'
+DB_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/musicweb?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = DB_URI
+
+SQLALCHEMY_BINDS = {
+    'studentdb': f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/studentdb?charset=utf8mb4'
+}
+
+
+MAIL_SERVER = "smtp.qq.com"
+MAIL_USE_SSL = True
+MAIL_PORT = 465
+MAIL_USERNAME = "2908842370@qq.com"
+MAIL_PASSWORD = "jykcfuutsbabdgcf"
+MAIL_DEFAULT_SENDER = "2908842370@qq.com"
+
+
