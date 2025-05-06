f16d099a247bf4ae18a07019c91b853bd04c196c
Dovahkiin2333-lnx <alanxu2333@outlook.com>
2025-05-06 14:26:05 +0100

    1

--- app.py
+++ app.py
@@ -26,38 +26,39 @@ socketio.init_app(app, cors_allowed_origins="*")
 migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'auth.login'
+login_manager.login_view = "auth.login"
 
 
 app.register_blueprint(auth_bp, url_prefix="/user/auth")
-app.register_blueprint(profile_bp, url_prefix='/user/profile')
+app.register_blueprint(profile_bp, url_prefix="/user/profile")
 app.register_blueprint(admin_bp, url_prefix="/admin")
 app.register_blueprint(upload_bp, url_prefix="/user/upload")
-app.register_blueprint(home_bp, url_prefix='/')
+app.register_blueprint(home_bp, url_prefix="/")
 app.register_blueprint(song_bp, url_prefix="/song")
 app.register_blueprint(message_bp, url_prefix="/user/profile")
 app.register_blueprint(search_bp, url_prefix="/")
 
-csrf.exempt(app.view_functions['message.send_message'])
-csrf.exempt(app.view_functions['home.category'])
-csrf.exempt(app.view_functions['song.like_song'])
-csrf.exempt(app.view_functions['song.dislike_song'])
-csrf.exempt(app.view_functions['profile.add_playlist'])
-csrf.exempt(app.view_functions['profile.delete_playlist'])
-csrf.exempt(app.view_functions['profile.rename_playlist'])
-csrf.exempt(app.view_functions['profile.publish_playlist'])
-csrf.exempt(app.view_functions['profile.set_private'])
-csrf.exempt(app.view_functions['profile.delete_song_route'])
+csrf.exempt(app.view_functions["message.send_message"])
+csrf.exempt(app.view_functions["home.category"])
+csrf.exempt(app.view_functions["song.like_song"])
+csrf.exempt(app.view_functions["song.dislike_song"])
+csrf.exempt(app.view_functions["profile.add_playlist"])
+csrf.exempt(app.view_functions["profile.delete_playlist"])
+csrf.exempt(app.view_functions["profile.rename_playlist"])
+csrf.exempt(app.view_functions["profile.publish_playlist"])
+csrf.exempt(app.view_functions["profile.set_private"])
+csrf.exempt(app.view_functions["profile.delete_song_route"])
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return UserModel.query.get(int(user_id))
 
+
 @app.context_processor
 def my_context_processor():
     return {"current_user": current_user}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, debug=True)
